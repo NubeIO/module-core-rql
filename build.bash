760a815184b06896ad8df616b791d4d56692cbf2
NubeDev <aidan@enviroservices.com.au>
2023-07-17 14:14:35 +1000

    added bash script

--- build.bash
+++ build.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 echo PATH $1/rubix-os
 echo ****MODULES****
 ls  $1/rubix-os/data/modules
 echo ****MODULES****
+
 go build -o module-core-rql && mv -f module-core-rql $1/rubix-os/data/modules && cd $1/rubix-os && bash build.bash
