5d589677a5eda785de6827974f621ed2b4894449
NubeDev <aidan@enviroservices.com.au>
2023-09-27 12:42:49 -0500

    added test for gmail

--- apirules/email.go
+++ apirules/email.go
@@ -2,9 +2,23 @@ package apirules
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/jordan-wright/email"
 	"net/smtp"
 )
+
+/*
+let body = {
+  To: ["test@nube-io.com"],
+  Subject: "test",
+  Message: "abc",
+  SenderAddress: "test@nube-io.com",
+  Token: "YOUR TOKEN",
+};
+
+RQL.Result = RQL.SendEmail(body);
+*/
 
 type Mail struct {
 	To            []string
@@ -16,27 +30,40 @@ type Mail struct {
 	Token         string
 }
 
-func (inst *RQL) sendEmail(body *Mail) error {
-	parsed, err := emailBody(body)
-	if err != nil {
-		return err
+func (inst *RQL) sendEmail(body *Mail) any {
+
+	inst.Log(body)
+	if body == nil {
+		return errors.New("email body can not be empty")
 	}
-	to := parsed.To
-	subject := parsed.Subject
-	message := parsed.Message
-	senderAddress := parsed.SenderAddress
-	password := parsed.Token
+
+	to := body.To
+	if len(to) <= 0 {
+		return errors.New("to address can not be empty")
+	}
+
+	subject := body.Subject
+	message := body.Message
+	senderAddress := body.SenderAddress
+	if senderAddress == "" {
+		return errors.New("sender address can not be empty")
+	}
+	password := body.Token
 	e := email.NewEmail()
 	e.From = senderAddress
 	e.To = to
-	e.Cc = parsed.To
-	e.Bcc = parsed.Bcc
+	e.Cc = body.To
+	e.Bcc = body.Bcc
 	e.Subject = subject
 	e.HTML = []byte(message)
-	return e.Send("smtp.gmail.com:587", smtp.PlainAuth("", senderAddress, password, "smtp.gmail.com"))
+	err := e.Send("smtp.gmail.com:587", smtp.PlainAuth("", senderAddress, password, "smtp.gmail.com"))
+	if err != nil {
+		return err
+	}
+	return fmt.Sprintf("email sent ok from: %s", senderAddress)
 }
 
-func (inst *RQL) SendEmail(body *Mail) error {
+func (inst *RQL) SendEmail(body *Mail) any {
 	return inst.sendEmail(body)
 
 }
