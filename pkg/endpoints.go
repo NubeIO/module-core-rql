3c2f11d151a4adaf9a2750ce5377fb08322ab316
NubeDev <aidan@enviroservices.com.au>
2023-10-17 08:44:09 -0500

    added curl example for serverless example

--- pkg/endpoints.go
+++ pkg/endpoints.go
@@ -102,8 +102,10 @@ RQL.Result = out;
     }
 }
 
+- add a new script called test and paste in the example code above
+- call the end point below as per the curl example
 
-curl -i -X POST -H "Content-Type: application/json" -d '{"body":{"a":100}}' http://0.0.0.0:1660/api/modules/module-core-rql/rules/run/test2
+curl -i -X POST -H "Content-Type: application/json" -d '{"body":{"a":100}}' http://0.0.0.0:1660/api/modules/module-core-rql/rules/run/test
 */
 func (inst *Module) ReuseRuleRun(b []byte, nameUUID string) ([]byte, error) {
 	start := time.Now()
